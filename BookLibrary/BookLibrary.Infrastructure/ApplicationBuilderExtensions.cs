d6af6be86f720f9afa1c07419fc4161fcf582a45
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-03-12 19:59:55 +0100

    Added method SeedCategories

--- BookLibrary/BookLibrary.Infrastructure/ApplicationBuilderExtensions.cs
+++ BookLibrary/BookLibrary.Infrastructure/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using BookLibrary.Data;
+using BookLibrary.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,7 +17,29 @@ namespace BookLibrary.Infrastructure
 
             data.Database.Migrate();
 
+            SeedCategories(data);
+
             return app;
+        }
+
+        private static void SeedCategories(BookLibraryDbContext data)
+        {
+            if (!data.Categories.Any())
+            {
+                return;
+            }
+
+            data.Categories.AddRange(new[]
+            {
+                new Category { Name = "Literary Fiction" },
+                new Category { Name = "Horror" },
+                new Category { Name = "Historical Fiction" },
+                new Category { Name = "Fantasy" },
+                new Category { Name = "Detective and Mystery" },
+                new Category { Name = "Comic Book or Graphic Novel" },
+                new Category { Name = "Classics" },
+                new Category { Name = "Action and Adventure" },
+            });
         }
     }
 }
