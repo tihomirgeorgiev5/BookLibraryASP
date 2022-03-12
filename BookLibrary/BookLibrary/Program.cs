8f8e9b1b649f423028d7a4c10c6599da7b349e6a
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-03-12 13:35:34 +0100

    cleaned the project

--- BookLibrary/BookLibrary/Program.cs
+++ BookLibrary/BookLibrary/Program.cs
@@ -6,11 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString(@"Server=DESKTOP-SB9MJ7T\SQLEXPRESS;Database=BookLibrary;Trusted_Connection=True;Integrated Security=True;");
+
 builder.Services.AddDbContext<BookLibraryDbContext>(options =>
     options.UseSqlServer(@"Server=DESKTOP-SB9MJ7T\SQLEXPRESS;Database=BookLibrary;Trusted_Connection=True;Integrated Security=True;"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+                  {
+                      options.Password.RequireDigit = false;
+                      options.Password.RequireLowercase = false;
+                      options.Password.RequireNonAlphanumeric = false;
+                      options.Password.RequireUppercase = false;
+                      })
     .AddEntityFrameworkStores<BookLibraryDbContext>();
 builder.Services.AddControllersWithViews();
 
@@ -24,21 +31,21 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseHttpsRedirection()
+   .UseStaticFiles()
+   .UseRouting()
+   .UseAuthentication()
+   .UseAuthorization();
 
-app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+   app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+    
 app.MapRazorPages();
 
 app.Run();
