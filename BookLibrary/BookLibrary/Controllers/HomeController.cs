b483d758890f04a764e6f5d06f582ffd13168c06
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-03-12 20:43:18 +0100

    Modified views

--- BookLibrary/BookLibrary/Controllers/HomeController.cs
+++ BookLibrary/BookLibrary/Controllers/HomeController.cs
@@ -1,32 +1,16 @@
-﻿using BookLibrary.Models;
+﻿using System.Diagnostics;
+using BookLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+
 
 namespace BookLibrary.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        public IActionResult Index() => View();
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        
     }
 }
